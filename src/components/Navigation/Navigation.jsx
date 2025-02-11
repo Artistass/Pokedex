13e5fd7bca0e7f12dedbb971cc0d9433d6ecbf24
Antanas Varanauskas <antifreezlt@gmail.com>
2025-02-11 18:16:07 +0200

    Changes

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -7,7 +7,7 @@ export default function Navigation() {
   return (
     <Box width="80%" margin="auto" padding="0" backgroundColor="#eeee" display="flex" justifyContent="space-between" alignItems="center">
       <Box display="flex" justifyContent="right" alignContent="center">
-        <Button variant="contained" onClick={() => navigate("/")} sx={{ margin: "1rem" }}>Home</Button>
+        <Button variant="contained" onClick={() => navigate("/")} sx={{ margin: "5rem" }}>Home</Button>
         <Button variant="contained" onClick={() => navigate("/favorites")} sx={{ margin: "1rem" }}>Favorites</Button>
       </Box>
         <Button variant="contained" onClick={() => navigate("/licence")} sx={{ margin: "1rem" }}>Licence</Button>
